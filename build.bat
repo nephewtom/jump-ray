ec46c611444bffdffd9a30239ee32dddb1943a79
Tomas Orti <tomas.orti.martin@ericsson.com>
2025-02-18 17:16:46 +0100

    Added raylib as submodule repo and changed build.bat file

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 del jump-ray.exe
-g++ jump-ray.cpp -o jump-ray.exe -Id:/playground/raylib/src -Ld:/playground/raylib/src -lraylib -lopengl32 -lgdi32 -lwinmm -Wall -Wextra -Wno-missing-field-initializers -g
+g++ jump-ray.cpp -o jump-ray.exe -I raylib/src -L raylib/src -lraylib -lopengl32 -lgdi32 -lwinmm -Wall -Wextra -Wno-missing-field-initializers -g
 .\jump-ray.exe
